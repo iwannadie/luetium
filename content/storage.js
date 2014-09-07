e6fa746115c2bc62b156b19650608d5eceffb346
iwannadie <rlagydus@gmail.com>
2014-09-07 05:10:32 -0400

    keep copy of options in localStorage for faster rendering

--- content/storage.js
+++ content/storage.js
@@ -4,18 +4,51 @@
   var listeners = {};
   chrome.storage.onChanged.addListener(function(changes) {
     for (var key in changes) {
+      var newValue = changes[key].newValue;
       var listener = listeners[key];
       if (listener) {
-        listener(changes[key].newValue);
+        listener(newValue);
       }
+      if (options) {
+        options[key] = newValue;
+      }
+      localStorage.setItem('luetium', JSON.stringify(options));
     }
   });
 
+  // An updated copy of options is kept in localStorage because
+  // of its synchronous API. With things that affect styling,
+  // it's important to be able to set this style right when the
+  // content is loaded, or before, rather than witnessing
+  // "jumpy" or "flashy" elements.
+  var options = localStorage.getItem('luetium');
+  if (options) {
+    try {
+      options = JSON.parse(options);
+    } catch (err) {
+      getStorage();
+    }
+  } else {
+    getStorage();
+  }
+
+  function getStorage() {
+    chrome.storage.sync.get(null, function(items) {
+      options = items;
+      localStorage.setItem('luetium', JSON.stringify(options));
+    });
+  }
+
   window.storage = {
     get: function(key, callback) {
-      chrome.storage.sync.get(key, function(items) {
-        callback(items[key]);
-      });
+      if (options && options[key]) {
+        callback(options[key]);
+      } else {
+        chrome.storage.sync.get(key, function(items) {
+          callback(items[key]);
+        });
+        getStorage();
+      }
       listeners[key] = callback;
     }
   };
