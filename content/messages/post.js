97a3b22722628cfa925612aff301c7b9acde7296
iwannadie <rlagydus@gmail.com>
2014-09-06 23:36:58 -0400

    separate sig from message without destroying elements

--- content/messages/post.js
+++ content/messages/post.js
@@ -5,22 +5,38 @@ window.Post = function($node) {
   var $message = this.$message = $node.getElementsByClassName('message')[0];
 
   // Separate the sig from the message.
-  var messageHTML = $message.innerHTML;
-  var belt = '\n---<br>\n';
-  var beltp = messageHTML.lastIndexOf(belt);
+  var $children = $message.childNodes;
+  var br = 0;
+  var sigIndex = -1;
+  for (var i = $children.length - 1; i >= 0 && br <= 3; i--) {
+    var $n = $children[i];
+    if ($n.tagName === 'BR') {
+      br++;
+    } else if ($n.nodeType === 3 && $n.nodeValue.trim() === '---') {
+      sigIndex = i;
+    }
+  }
 
-  if (beltp > -1) {
-    var $mws = this.$messageWithoutSig =
-      dom.createDiv(messageHTML.slice(0, beltp));
+  if (sigIndex > -1) {
+    var $mws = this.$messageWithoutSig = dom.createDiv('');
     $mws.className = 'luetium-message';
-    var $belt = this.$belt = dom.createDiv('<div>---</div>');
+    for (i = 0; i < sigIndex; i++) {
+      $mws.appendChild($children[0]);
+    }
+
+    var $belt = this.$belt = dom.createDiv('<div></div>');
     $belt.className = 'secret';
     $belt.firstChild.className = 'luetium-belt';
-    var $sig = this.$sig = dom.createDiv(
-        '<div>' + messageHTML.slice(beltp + belt.length) + '</div>');
+    $belt.children[0].appendChild($children[0]);
+    $message.removeChild($children[0]);
+
+    var $sig = this.$sig = dom.createDiv('<div></div>');
     $sig.className = 'secret';
     $sig.firstChild.className = 'luetium-sig';
-    $message.innerHTML = '';
+    for (i = $children.length - 1; i >= 0; i--) {
+      $sig.children[0].appendChild($children[0]);
+    }
+
     $message.appendChild($mws);
     $message.appendChild($belt);
     $message.appendChild($sig);
